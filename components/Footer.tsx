2d26a8dd6394e988536bcbf460b6af5e2ae55fcb
arielmoguillansky <arielmogui92@gmail.com>
2022-10-03 14:12:21 -0300

    v3

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,33 +7,25 @@ const Footer = () => {
   return (
     <footer>
       <div className="contact-block">
-        <h2>Ariel Moguillansky</h2>
-        <div>
-          <CustomLink
-            customClass="mr-4"
-            text="Github"
-            url="https://github.com/arielmoguillansky"
-          />
-          <CustomLink
-            text="LinkedIn"
-            url="https://www.linkedin.com/in/arielmoguillansky/"
-          />
-        </div>
-      </div>
-      <div className={`language-block ${locale}`}>
-        <span>{locale === "es-ES" ? "Sitio en " : "Site in"}</span>
-        <div className={`language-selector`}>
-          <Link href={asPath} locale="es-ES" scroll={false}>
-            <a href="" className="language-link es-link">
-              {locale === "es-ES" ? "español" : "spanish"}
-            </a>
-          </Link>
-          <Link href={asPath} locale="en-US" scroll={false}>
-            <a href="" className="language-link us-link">
-              {locale === "es-ES" ? "inglés" : "english"}
-            </a>
-          </Link>
-        </div>
+        <span>Handcrafted by Ariel Moguillansky</span>
+        <span>
+          Made with <i className="icon-github" /> &amp; NextJs
+        </span>
+        {/* <div className={`language-block ${locale}`}>
+          <span>{locale === "es-ES" ? "Sitio en " : "Site in"}</span>
+          <div className={`language-selector`}>
+            <Link href={asPath} locale="es-ES" scroll={false}>
+              <a href="" className="language-link es-link">
+                {locale === "es-ES" ? "español" : "spanish"}
+              </a>
+            </Link>
+            <Link href={asPath} locale="en-US" scroll={false}>
+              <a href="" className="language-link us-link">
+                {locale === "es-ES" ? "inglés" : "english"}
+              </a>
+            </Link>
+          </div>
+        </div> */}
       </div>
     </footer>
   );
